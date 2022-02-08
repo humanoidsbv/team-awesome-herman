5a4998d7afeec48efea0a061138d9dcaf69e297c
HERMAN VULKERS <hermanvulkers@gmail.com>
2022-02-08 09:17:39 +0100

    Improved styling

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const menuItems = document.querySelector(".menu");
 
 menuButton.addEventListener('click', () => {
 
-  console.log('Test');
-
   if (menuItems.style.display === "none") {
     menuItems.style.display = "block";
+    document.querySelector(".menu-button-icon").src = "assets/icons/Close.svg";
   } else {
     menuItems.style.display = "none";
+    document.querySelector(".menu-button-icon").src = "assets/icons/Hamburger.svg";
   }
   
   });
